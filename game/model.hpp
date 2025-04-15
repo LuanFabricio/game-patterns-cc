d3e3bfb4fd4a9d80cd610d0e634e49df3af8666f
LuanFabricio <luanlimaleite@hotmail.com>
2025-04-14 23:43:07 -0300

    feat: adds TerrainModel

--- game/model.hpp
+++ game/model.hpp
@@ -1,6 +1,7 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
 
+#include <cstdio>
 #include <raylib.h>
 
 namespace game {
@@ -8,6 +9,22 @@ namespace game {
 		Mesh mesh;
 
 		EntityModel(Mesh _mesh) : mesh(_mesh) { }
+	};
+
+	struct TerrainModel {
+		Mesh mesh;
+		Material material;
+		Texture texture;
+		Model model;
+
+		TerrainModel(Mesh _mesh, Material _mat) : mesh(_mesh), material(_mat) { }
+		TerrainModel(Mesh _mesh, Texture _texture) : mesh(_mesh), texture(_texture) {
+			this->material = LoadMaterialDefault();
+
+			this->model = LoadModelFromMesh(_mesh);
+			this->model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = _texture;
+			// model.texture = texture;
+		}
 	};
 };
 
