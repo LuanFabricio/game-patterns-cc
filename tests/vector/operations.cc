73cff545ba0cf1421f698d5027e78af47c775ca2
LuanFabricio <luanlimaleite@hotmail.com>
2025-04-15 00:12:58 -0300

    tests: adds an example with binary multiplication with a vector and a scalar

--- tests/vector/operations.cc
+++ tests/vector/operations.cc
@@ -12,3 +12,13 @@ TEST(Vector, VectorShouldMultiplyWithAValue)
 	EXPECT_EQ(v.y, 5.0f);
 	EXPECT_EQ(v.z, 1.0f);
 }
+
+TEST(Vector, VectorShouldMultiplyBinaryWithAValue)
+{
+	game::Vector v1(1.0f, 0.5f, 0.1f);
+	game::Vector v2 = v1 * -1.0f;
+
+	EXPECT_EQ(v2.x, -v1.x);
+	EXPECT_EQ(v2.y, -v1.y);
+	EXPECT_EQ(v2.z, -v1.z);
+}
