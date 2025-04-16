8a8015de57b2be1292b8cf34a297b0e3c0e9f27d
LuanFabricio <luanlimaleite@hotmail.com>
2025-04-16 00:14:35 -0300

    test: moves test files to tests/

---
+++ tests/game_actor.cc
@@ -0,0 +1,39 @@
+#include <cstdio>
+
+#include "raylib.h"
+#include "gtest/gtest.h"
+
+#include "../game/game_actor.hpp"
+#include "../game/model.hpp"
+#include "../game/vector.hpp"
+
+TEST(GameActorTest, MoveShouldUpdatePlayerPosition)
+{
+	game::EntityModel model(Mesh{});
+	game::GameActor ga(
+		model,
+		game::Vector(0, 0, 0),
+		game::Vector(100.0f, 50.0f, 100.0f)
+	);
+
+	ga.move(1.0f);
+
+	EXPECT_TRUE(ga.position.x > 0);
+	EXPECT_TRUE(ga.position.y > 0);
+	EXPECT_TRUE(ga.position.z > 0);
+}
+
+TEST(GameActorTest, RotateShouldUpdatePlayerAngle)
+{
+	game::EntityModel model(Mesh{});
+	game::GameActor ga(
+		model,
+		game::Vector(0, 0, 0),
+		game::Vector(100.0f, 50.0f, 100.0f)
+	);
+
+	const float old_angle = ga.angle;
+	ga.rotate(1.0f, 10);
+
+	EXPECT_NE(old_angle, ga.angle);
+}
--- tests/vector.cc
+++ tests/vector.cc
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 
-#include "../../game/vector.hpp"
+#include "../game/vector.hpp"
 
 TEST(Vector, VectorShouldMultiplyWithAValue)
 {
