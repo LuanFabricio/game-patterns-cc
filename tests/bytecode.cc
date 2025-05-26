feef7caad1da39467e8ceeac980cece6cae86960
Luan Fabrício <luanlimaleite@hotmail.com>
2025-05-26 00:06:06 -0300

    tests: updates CMD_MOVE tests to use new args

--- tests/bytecode.cc
+++ tests/bytecode.cc
@@ -6,8 +6,13 @@
 TEST(Bytecode, CMD_MOVE)
 {
   const uint8_t bytecodeArr[] = {
-    game::BYTECODE_CMD_STACK, 0x0,
-    game::BYTECODE_CMD_STACK, 0xA,
+    game::BYTECODE_CMD_STACK, 0xA, // Speed
+    game::BYTECODE_CMD_STACK, 0x1, // Direction Z
+    game::BYTECODE_CMD_STACK, 0x1, // Direction Y
+    game::BYTECODE_CMD_STACK, 0x1, // Direction X
+    game::BYTECODE_CMD_STACK, 0xF, // Target Z
+    game::BYTECODE_CMD_STACK, 0xA, // Target Y
+    game::BYTECODE_CMD_STACK, 0x0, // Target X
     game::BYTECODE_CMD_MOVE,
     0x12
   };
@@ -18,10 +23,11 @@ TEST(Bytecode, CMD_MOVE)
   game::GameActor gameActor(model, game::Vector(0, 0, 0));
 
   behavior.execute(gameActor, 1);
-  gameActor.move(1);
 
   printf("%f, %f, %f\n", gameActor.direction.x, gameActor.direction.y, gameActor.direction.z);
-  EXPECT_EQ(gameActor.position.x, 10);
+  EXPECT_EQ(gameActor.position.x, 0x00);
+  EXPECT_EQ(gameActor.position.y, 0x0A);
+  EXPECT_EQ(gameActor.position.z, 0x0A);
 }
 
 TEST(Bytecode, CMD_STACK)
