4d842ed2db0f6dc649f8874ccf7d270735c31838
Luan Fabrício <luanlimaleite@hotmail.com>
2025-05-25 22:32:42 -0300

    tests: adds CMD_STACK test

--- tests/bytecode.cc
+++ tests/bytecode.cc
@@ -3,7 +3,7 @@
 
 #include "../game/bytecode/bytecode.hpp"
 
-TEST(Bytecode, A)
+TEST(Bytecode, CMD_MOVE)
 {
   const uint8_t bytecodeArr[] = {
     game::BYTECODE_CMD_STACK, 0x0,
@@ -22,4 +22,17 @@ TEST(Bytecode, A)
 
   printf("%f, %f, %f\n", gameActor.direction.x, gameActor.direction.y, gameActor.direction.z);
   EXPECT_EQ(gameActor.position.x, 10);
+}
+
+TEST(Bytecode, CMD_STACK)
+{
+  const uint8_t bytecodeArr[] = {
+    game::BYTECODE_CMD_STACK, 0xFF,
+    game::BYTECODE_CMD_STACK, 0xAA,
+  };
+
+  game::Bytecode behavior(bytecodeArr, sizeof(bytecodeArr));
+
+  EXPECT_EQ(behavior.getStack(1), bytecodeArr[3]);
+  EXPECT_EQ(behavior.getStack(0), bytecodeArr[1]);
 }
